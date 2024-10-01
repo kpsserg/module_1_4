ba184bece244d699f7663eea1d77cbddadd3ddfa
kpsserg <kpsserg@gmail.com>
2024-10-01 21:34:28 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('ведите текст. Желательно выражение или предложение ')
+print(type(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+print('Количество символов в строке равняется: ', len(my_string))
+
